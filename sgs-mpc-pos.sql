bb0e29b7dcb18ef791c2862a6ae430a65fbdc33b
nantenangel <nantenangel@gmail.com>
2016-05-11 02:27:50 +0800

    Update sgs-mpc-pos.sql

--- sgs-mpc-pos.sql
+++ sgs-mpc-pos.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: May 07, 2016 at 05:14 AM
+-- Generation Time: May 10, 2016 at 08:26 PM
 -- Server version: 5.6.26
 -- PHP Version: 5.6.12
 
@@ -28,15 +28,18 @@ SET time_zone = "+00:00";
 
 CREATE TABLE IF NOT EXISTS `employeedetails` (
   `EmployeeID` varchar(16) NOT NULL,
-  `SurrName` varchar(50) NOT NULL,
+  `SurName` varchar(50) NOT NULL,
   `GivenName` varchar(30) NOT NULL,
   `MiddleName` varchar(30) NOT NULL,
+  `Gender` varchar(10) NOT NULL,
+  `DateOfBirth` varchar(50) NOT NULL,
+  `Address` varchar(256) NOT NULL,
+  `ContactNo` varchar(20) NOT NULL,
+  `E-mail` varchar(64) NOT NULL,
+  `Position` varchar(50) NOT NULL,
   `Department` varchar(50) NOT NULL,
   `Branch` varchar(50) NOT NULL,
-  `Position` varchar(50) NOT NULL,
-  `Status` varchar(20) NOT NULL,
-  `Address` varchar(256) NOT NULL,
-  `ContactNo` varchar(20) NOT NULL
+  `Status` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -58,10 +61,11 @@ CREATE TABLE IF NOT EXISTS `employeelogin` (
 --
 
 CREATE TABLE IF NOT EXISTS `purchaseorderdetail` (
-  `ItemID` varchar(7) NOT NULL,
-  `PurchaseRequestID` varchar(6) NOT NULL,
-  `Item` varchar(30) NOT NULL,
-  `Quantity` int(11) NOT NULL
+  `OrderItemID` varchar(7) NOT NULL,
+  `OrderID` varchar(6) NOT NULL,
+  `OrderItem` varchar(50) NOT NULL,
+  `OrderQuantity` int(11) NOT NULL,
+  `OrderPrice` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -71,9 +75,39 @@ CREATE TABLE IF NOT EXISTS `purchaseorderdetail` (
 --
 
 CREATE TABLE IF NOT EXISTS `purchaseorderheader` (
-  `PurchaseRequestID` varchar(6) NOT NULL,
+  `OrderID` varchar(6) NOT NULL,
   `EmployeeID` varchar(16) NOT NULL,
-  `RequestDate` varchar(20) NOT NULL
+  `OrderStatus` varchar(10) NOT NULL,
+  `RequestID` varchar(6) NOT NULL,
+  `DeliveryDate` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `purchaserequestdetail`
+--
+
+CREATE TABLE IF NOT EXISTS `purchaserequestdetail` (
+  `RequestItemID` varchar(7) NOT NULL,
+  `RequestID` varchar(6) NOT NULL,
+  `RequestItem` varchar(50) NOT NULL,
+  `RequestQuantity` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `purchaserequestheader`
+--
+
+CREATE TABLE IF NOT EXISTS `purchaserequestheader` (
+  `RequestID` varchar(6) NOT NULL,
+  `EmployeeID` varchar(16) NOT NULL,
+  `RequestDate` varchar(30) NOT NULL,
+  `RequestStatus` varchar(30) NOT NULL,
+  `DueDate` varchar(30) NOT NULL,
+  `RequestRemarks` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -91,6 +125,12 @@ ALTER TABLE `employeedetails`
 --
 ALTER TABLE `employeelogin`
   ADD UNIQUE KEY `EmployeeID` (`EmployeeID`);
+
+--
+-- Indexes for table `purchaseorderdetail`
+--
+ALTER TABLE `purchaseorderdetail`
+  ADD PRIMARY KEY (`OrderItemID`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
